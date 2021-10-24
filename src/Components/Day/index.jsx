ce66094ba78364f032586871e7912bd2ce834291
zerofille <nstrdnv@gmai.com>
2021-10-24 12:21:48 +0300

    Add selected days styles

--- src/Components/Day/index.jsx
+++ src/Components/Day/index.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
-import { format, addDays, startOfWeek } from "date-fns";
+import { format, isSameDay, isSameMonth } from "date-fns";
 import DayContext from "../../Context";
 import styles from "./style.module.scss";
+import cx from "classnames";
 
 function Day({ day }) {
   const [currentDay, setCurrentDay] = useContext(DayContext);
@@ -9,8 +10,17 @@ function Day({ day }) {
     setCurrentDay(day);
   };
 
+  const selectedDay = {
+    [styles.selected]: isSameDay(day, currentDay),
+  };
+  const hiddenDays = {
+    [styles.hiddenDays]: isSameMonth(day, currentDay),
+  };
   return (
-    <div onClick={dayHandler} className={styles.day}>
+    <div
+      onClick={dayHandler}
+      className={cx(styles.notSelected, selectedDay, hiddenDays)}
+    >
       {format(day, "d")}
     </div>
   );
